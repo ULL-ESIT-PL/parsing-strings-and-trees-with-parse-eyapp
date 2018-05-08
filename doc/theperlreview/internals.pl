12fa7138353675156ac804c18f6b465c9d5e38e0
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2018-05-08 13:09:47 +0100

    updated icons

--- doc/theperlreview/internals.pl
+++ doc/theperlreview/internals.pl
@@ -1,81 +1,81 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2002-2-1 (1.71)
 # Associate internals original text with physical files.
 
 
 $key = q/cite_desarmenien/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_simoes/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_parrot/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:codegeneration/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node10.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:machineindependentoptimizations/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node8.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_briggs98register/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:introduction/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node1.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:parrot/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:phases/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node3.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:addressassign/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node9.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_Tambouras/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:syntacticanalysis/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node5.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_yacc/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:output/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node12.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_conwayrd/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:lexicalanalysis/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:peephole/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node11.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_randal04/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/end:of:file/;
-$ref_files{$key} = "$dir".q|theperlreview.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- doc/theperlreview/labels.pl
+++ doc/theperlreview/labels.pl
@@ -1,87 +1,87 @@
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2002-2-1 (1.71)
 # Associate labels original text with physical files.
 
 
 $key = q/cite_desarmenien/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_simoes/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_parrot/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:codegeneration/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node10.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:machineindependentoptimizations/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node8.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_briggs98register/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:introduction/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node1.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:parrot/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node2.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:phases/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node3.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:addressassign/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_Tambouras/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:syntacticanalysis/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node5.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_yacc/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:output/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node12.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_conwayrd/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:lexicalanalysis/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node4.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/section:peephole/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node11.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_randal04/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/end:of:file/;
-$external_labels{$key} = "$URL/" . q|theperlreview.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 1;
 
 
-# LaTeX2HTML 2008 (1.71)
+# LaTeX2HTML 2002-2-1 (1.71)
 # labels from external_latex_labels array.
 
 
